1816bee2052ff7729be6302d33033ae39047c0a5
vladimirbutyrskii <vlaver@mail.ru>
2026-05-23 23:47:59 +0300

    add ads/serializers.py and ads/filters.py

---
+++ ads/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+
+from .models import Ad
+
+
+class AdFilter(django_filters.FilterSet):
+    """Фильтр объявлений по названию."""
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+
+    class Meta:
+        model = Ad
+        fields = ['title']
---
+++ ads/serializers.py
@@ -0,0 +1,89 @@
+from rest_framework import serializers
+
+from .models import Ad, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор отзыва."""
+    author_email = serializers.EmailField(source='author.email', read_only=True)
+    author_name = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'text',
+            'author',
+            'author_email',
+            'author_name',
+            'ad',
+            'created_at',
+        )
+        read_only_fields = ('id', 'author', 'created_at')
+
+    def get_author_name(self, obj):
+        return f'{obj.author.first_name} {obj.author.last_name}'
+
+
+class AdListSerializer(serializers.ModelSerializer):
+    """Сериализатор объявления для списка (без полного описания)."""
+    author_email = serializers.EmailField(source='author.email', read_only=True)
+    author_name = serializers.SerializerMethodField(read_only=True)
+    comments_count = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Ad
+        fields = (
+            'id',
+            'title',
+            'price',
+            'author_email',
+            'author_name',
+            'comments_count',
+            'created_at',
+        )
+        read_only_fields = ('id', 'author', 'created_at')
+
+    def get_author_name(self, obj):
+        return f'{obj.author.first_name} {obj.author.last_name}'
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+
+class AdDetailSerializer(serializers.ModelSerializer):
+    """Сериализатор объявления для детального просмотра (с описанием и отзывами)."""
+    author_email = serializers.EmailField(source='author.email', read_only=True)
+    author_name = serializers.SerializerMethodField(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ad
+        fields = (
+            'id',
+            'title',
+            'price',
+            'description',
+            'author',
+            'author_email',
+            'author_name',
+            'comments',
+            'created_at',
+        )
+        read_only_fields = ('id', 'author', 'created_at')
+
+    def get_author_name(self, obj):
+        return f'{obj.author.first_name} {obj.author.last_name}'
+
+
+class AdCreateUpdateSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания и редактирования объявления."""
+
+    class Meta:
+        model = Ad
+        fields = (
+            'title',
+            'price',
+            'description',
+        )
+        
