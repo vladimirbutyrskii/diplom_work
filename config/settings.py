1858bcfadbf8884bcb5f33c66c9522a949714f31
vladimirbutyrskii <vlaver@mail.ru>
2026-05-27 23:55:00 +0300

    editing settings

--- config/settings.py
+++ config/settings.py
@@ -177,6 +177,6 @@ SWAGGER_SETTINGS = {
 
 # ===================================================
 # НАСТРОЙКИ ДЛЯ РАБОТЫ ЗА ПРОКСИ (Docker, Yandex Cloud)
-# =================================================
+# ===================================================
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
